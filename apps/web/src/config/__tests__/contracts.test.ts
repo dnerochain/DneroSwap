1932714a1bebb308b42905ff99a780a10877a208
Muroko <murokomail@gmail.com>
2024-03-30 20:35:15 -0400

    CHANGE: chainID to dnero chainID

--- apps/web/src/config/__tests__/contracts.test.ts
+++ apps/web/src/config/__tests__/contracts.test.ts
@@ -4,8 +4,8 @@ import contracts from 'config/constants/contracts'
 
 describe('Config contracts', () => {
   it.each(map(contracts, (contract, key) => [key, contract]))('Contract %s has a unique address', (key, contract) => {
-    if (contract[56]) {
-      const duplicates = filter(contracts, (c) => contract[56] === c[56])
+    if (contract[5647]) {
+      const duplicates = filter(contracts, (c) => contract[5647] === c[5647])
       expect(duplicates).toHaveLength(1)
     }
   })
